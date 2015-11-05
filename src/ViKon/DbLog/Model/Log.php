507d005ae1f1770dc2044b97f8ad1556e7ce56b1
Kovács Vince <vincekovacs@hotmail.com>
2015-11-05 12:16:24 +0100

    Initial commit

---
+++ src/ViKon/DbLog/Model/Log.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ViKon\DbLog\Model;
+
+use ViKon\Auth\Database\Eloquent\Model;
+
+/**
+ * Class Log
+ *
+ * @package ViKon\DbLog\Model
+ *
+ * @author  Kovács Vince<vincekovacs@hotmail.com>
+ *
+ * @property int            $id
+ * @property string         $message
+ * @property string         $context
+ * @property int            $level
+ * @property string         $channel
+ * @property \Carbon\Carbon $datetime
+ * @property string         $extra
+ */
+class Log extends Model
+{
+    const TABLE_NAME = 'logs';
+
+    const FIELD_ID         = 'id';
+    const FIELD_MESSAGE    = 'message';
+    const FIELD_CONTEXT    = 'context';
+    const FIELD_LEVEL      = 'level';
+    const FIELD_CHANNEL    = 'channel';
+    const FIELD_CREATED_AT = 'created_at';
+    const FIELD_EXTRA      = 'extra';
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->table      = static::TABLE_NAME;
+        $this->timestamps = false;
+        $this->dates      = [
+            static::FIELD_CREATED_AT,
+        ];
+
+        parent::__construct($attributes);
+    }
+}
